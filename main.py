891e4ba7bf18b2d3f6089fbf9b817e6849ee35b4
synaxxkhushi <149578930+synaxxkhushi@users.noreply.github.com>
2024-05-18 16:12:30 +0530

    Update main.py

--- main.py
+++ main.py
@@ -121,12 +121,12 @@ async def unban_user(m: UpdateNewMessage):
 async def stats_command(m: UpdateNewMessage):
     if m.is_group or m.is_channel:
         return
-    check_if = await is_user_on_chat(bot, f"@DextiNBots", m.peer_id)
+    check_if = await is_user_on_chat(bot, f"@synaxnetwork", m.peer_id)
     if not check_if:
-        return await m.reply(f"Please join @DextiNBots then send me the link again.")
-    check_if = await is_user_on_chat(bot, f"@TeamDextiN", m.peer_id)
+        return await m.reply(f"Please join @synaxnetwork then send me the link again.")
+    check_if = await is_user_on_chat(bot, f"@synaxbots", m.peer_id)
     if not check_if:
-        return await m.reply(f"Please join @TeamDextiN then send me the link again.")
+        return await m.reply(f"Please join @synaxbots then send me the link again.")
 
     uptime = convert_seconds(time.time() - bot_start_time)
     message_count = get_message_count()
@@ -180,19 +180,19 @@ async def start(m: UpdateNewMessage):
         )
         db.sadd("new_users", str(user_id))
 
-    check_if = await is_user_on_chat(bot, f"@DextiNBots", m.peer_id)
+    check_if = await is_user_on_chat(bot, f"@synaxnetwork", m.peer_id)
     if not check_if:
-        return await m.reply(f"Please join @DextiNBots then send me the link again.")
-    check_if = await is_user_on_chat(bot, f"@TeamDextiN", m.peer_id)
+        return await m.reply(f"Please join @synaxnetwork then send me the link again.")
+    check_if = await is_user_on_chat(bot, f"@synaxbots", m.peer_id)
     if not check_if:
-        return await m.reply(f"Please join @TeamDextiN then send me the link again.")
+        return await m.reply(f"Please join @synaxbots then send me the link again.")
     await m.reply(
         reply_text,
         buttons=[
             [
-                Button.url("Update Channel", "https://t.me/DextiNBots"),
+                Button.url("Update Channel", "https://t.me/synaxnetwork"),
                 Button.url(
-                    "Repo", "https://github.com/TheDextiN/Terabox-Downloader-Bot"
+                    "Update Bots", "https://t.me/synaxbots"
                 ),
             ]
         ],
@@ -233,12 +233,12 @@ async def broadcast(m: UpdateNewMessage):
 async def help_command(m: UpdateNewMessage):
     if m.is_group or m.is_channel:
         return
-    check_if = await is_user_on_chat(bot, f"@DextiNBots", m.peer_id)
+    check_if = await is_user_on_chat(bot, f"@synaxnetwork", m.peer_id)
     if not check_if:
-        return await m.reply(f"Please join @DextiNBots then send me the link again.")
-    check_if = await is_user_on_chat(bot, f"@TeamDextiN", m.peer_id)
+        return await m.reply(f"Please join @synaxnetwork then send me the link again.")
+    check_if = await is_user_on_chat(bot, f"@synaxbots", m.peer_id)
     if not check_if:
-        return await m.reply(f"Please join @TeamDextiN then send me the link again.")
+        return await m.reply(f"Please join @synaxbots then send me the link again.")
     help_text = """
 Available commands:
 
@@ -253,9 +253,9 @@ Available commands:
         parse_mode="markdown",
         buttons=[
             [
-                Button.url("Updates", "https://t.me/DextiNBots"),
+                Button.url("Updates", "https://t.me/synaxnetwork"),
                 Button.url(
-                    "Repo", "https://github.com/TheDextiN/Terabox-Downloader-Bot"
+                    "Bots", "https://t.me/synaxbots"
                 ),
             ]
         ],
@@ -291,14 +291,14 @@ async def handle_message(m: Message):
     url = get_urls_from_string(m.text)
     if not url:
         return await m.reply("Please enter a valid url.")
-    check_if = await is_user_on_chat(bot, "@DextiNBots", m.peer_id)
+    check_if = await is_user_on_chat(bot, "@synaxnetwork", m.peer_id)
     if not check_if:
-        return await m.reply("Please join @DextiNBots  then send me the link again.")
-    check_if = await is_user_on_chat(bot, "@TeamDextiN", m.peer_id)
+        return await m.reply("Please join @synaxnetwork  then send me the link again.")
+    check_if = await is_user_on_chat(bot, "@synaxbots", m.peer_id)
     if not check_if:
-        return await m.reply("Please join @TeamDextiN then send me the link again.")
+        return await m.reply("Please join @synaxbots then send me the link again.")
     is_spam = db.get(m.sender_id)
-    if is_spam and m.sender_id not in [6791744215]:
+    if is_spam and m.sender_id not in [6231550362]:
         return await m.reply("You are spamming. Please wait a 1 minute and try again.")
     hm = await m.reply("Sending you the media wait...")
     count = db.get(f"check_{m.sender_id}")
@@ -356,7 +356,7 @@ async def handle_message(m: Message):
         return await hm.edit(
             f"Sorry! File is not supported for now. I can download only .mp4, .mkv, .webm, .ts, .mov, .hevc, .png, .jpg, .jpeg files."
         )
-    if int(data["sizebytes"]) > 524288000 and m.sender_id not in [6791744215]:
+    if int(data["sizebytes"]) > 524288000 and m.sender_id not in [6231550362]:
         return await hm.edit(
             f"Sorry! File is too big. I can download only 500MB and this file is of {data['size']} ."
         )
@@ -484,5 +484,5 @@ Size: **{data["size"]}**
 bot.start(bot_token=BOT_TOKEN)
 print("Bot started!")
 print(f"This bot is connected to {BOT_USERNAME}.")
-print("This bot is deployed by @DextiNBots kindly join this channel for more updates.")
+print("This bot is deployed by @synaxnetwork kindly join this channel for more updates.")
 bot.run_until_disconnected()
