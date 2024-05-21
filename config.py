0a2fd3f7c57bd663c08ec93944702f6713d8556b
synaxxkhushi <149578930+synaxxkhushi@users.noreply.github.com>
2024-05-21 12:59:23 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,15 +2,15 @@ from os import environ
 
 
 # BOT CONFIG
-API_ID = environ.get("API_ID", 123456)  # api id
-API_HASH = environ.get("API_HASH", "")  # api hash
-BOT_TOKEN = environ.get("BOT_TOKEN", "")  # bot token
+API_ID = environ.get("API_ID", 28542813)  # api id
+API_HASH = environ.get("API_HASH", "02ce7c339f7776844ff4ab03da338ccd")  # api hash
+BOT_TOKEN = environ.get("BOT_TOKEN", "7150456941:AAHpwRti35c-XCcZvSpyNDgr4lvaH0io6nI")  # bot token
 
 # REDIS
-REDIS_HOST = environ.get("REDIS_HOST", "localhost")  # redis host uri
-REDIS_PORT = environ.get("REDIS_PORT", 12345)  # redis port
+REDIS_HOST = environ.get("REDIS_HOST", "redis-10693.c278.us-east-1-4.ec2.redns.redis-cloud.com")  # redis host uri
+REDIS_PORT = environ.get("REDIS_PORT", 10693)  # redis port
 REDIS_PASSWORD = environ.get(
-    "REDIS_PASSWORD", "q8vcRgGfoZgwKT6irvJ6AixPs1lFZdW8"
+    "REDIS_PASSWORD", "aLYDdunvCU0I0wY3Jrq6s3ehqabHl3Qf"
 )  # redis password
 
 
@@ -22,4 +22,4 @@ DUMP_CHANNEL = -1002002020654
 
 
 # Config
-COOKIE = environ.get("COOKIE", "")
+COOKIE = environ.get("COOKIE", "PANWEB=1; csrfToken=Aib-iThW-6-vc1tk3h76BHvZ; browserid=VHTZhOscJptCPoAVrDLdoD1gE4lErsXeDB_Elcc-lc9HczX1PYC3w_9zDIQ=; lang=en; bid_n=18f753c1c3635204fd4207; ab_sr=1.0.1_MDljNmU5NWRlMTlkN2RlOWMzNzVhNzI1MmZlMWE5NTQzODQzM2IwZDNjZGI0ODVlM2FmMDhmMWMxMDVjMDc0NDI0YzMyMjFjZTIyYTJhNjczN2NhZGMzYWQxYzQwZGQ1YWRiOWI3ZjU4NWQ3MDZiY2NjMDM5NGRlZWU5YjNhYzRkMmVjYmFkZGE0ZTA5YjRmNmRlMDg3Y2IwYzA3NmVhZg==; ndus=Yz_k7K3teHuiq2aTON0jEAxh4uwhaNMK0pAXVnb-; ndut_fmt=67039EAB0C881A957473530301AC907BE9BC8FD2BB007C2D112CF229CBE20EFA")
